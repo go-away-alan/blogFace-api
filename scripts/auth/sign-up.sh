42da008f3c6f3a3c554e70860c0e070b58a1d9d0
andyjzhong <andy.zhong@libertymutual.com>
2017-05-15 20:11:00 -0400

    Added Sign Up Curl Script
    Successfully created a curl script for authorizing user sign-up. Was
    able to run the curl script and received 201 Created feedback from node
    server.

--- scripts/auth/sign-up.sh
+++ scripts/auth/sign-up.sh
@@ -1,7 +1,12 @@
+#sh scripts/auth/sign-up.sh
+
 #!/bin/bash
 
 API="http://localhost:4741"
 URL_PATH="/sign-up"
+# AZ: Added EMAIL and PASSWORD in order to speed up the curl script testing process.
+EMAIL="admin"
+PASSWORD="admin"
 
 curl "${API}${URL_PATH}" \
   --include \
