ea44540790feab7c6d4249a432e766f133c86d92
andyjzhong <andy.zhong@libertymutual.com>
2017-05-16 09:48:54 -0400

    Retested feature after adding secret key
    Sign in functionality wasn't working previously without a local Secret
    Key. Once a Secret Key was added, the curl scripts for authentication
    all works. Just be sure to add in a valid token and id for particular
    actions.

--- scripts/auth/change-password.sh
+++ scripts/auth/change-password.sh
@@ -7,8 +7,8 @@ URL_PATH="/change-password"
 # AZ: Added OLDPW, NEWPW, ID, and TOKEN in order to speed up the curl script testing process. Be sure to pass in a valid TOKEN.
 OLDPW="admin"
 NEWPW="admin"
-TOKEN="insertValidTokenHere"
-ID="insertValidIdHere"
+TOKEN="jTJlpbfZvMk9uMsI5jnCZXmfXZ+Mt0/OSclqH0vj/68=--pFLYwVWxXhQsPKMoG9lpO8ngRVDCbi2cJk9Ofd6OihI="
+ID="591a43534eacfb88c235956e"
 
 curl "${API}${URL_PATH}/${ID}" \
   --include \
--- scripts/auth/sign-out.sh
+++ scripts/auth/sign-out.sh
@@ -5,8 +5,8 @@
 API="http://localhost:4741"
 URL_PATH="/sign-out"
 # AZ: Added ID and TOKEN in order to speed up the curl script testing process. Be sure to pass in a valid TOKEN and ID.
-TOKEN="insertValidTokenHere"
-ID="insertValidIdHere"
+TOKEN="QgjuyDgNw+o7pRdzSd/zTLLJuHLn5wFo3Bh3V48MJLc=--5RCfFS1wiwENsaTcvwMwSrlKnn+kK9m+ev935EuJbJk="
+ID="591a43534eacfb88c235956e"
 
 curl "${API}${URL_PATH}/${ID}" \
   --include \
