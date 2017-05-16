0d1840c8633dc2d327525da8954f6f2bdb6acb95
andyjzhong <andy.zhong@libertymutual.com>
2017-05-15 21:23:58 -0400

    Added Change Password Curl Script
    Set up the initial curl script for changing passwords. Waiting on
    Sign-in functionality to fully test.

--- scripts/auth/change-password.sh
+++ scripts/auth/change-password.sh
@@ -1,7 +1,14 @@
+#sh scripts/auth/change-password.sh
+
 #!/bin/bash
 
 API="http://localhost:4741"
 URL_PATH="/change-password"
+# AZ: Added OLDPW, NEWPW, ID, and TOKEN in order to speed up the curl script testing process. Be sure to pass in a valid TOKEN.
+OLDPW="admin"
+NEWPW="admin"
+TOKEN="insertValidTokenHere"
+ID="insertValidIdHere"
 
 curl "${API}${URL_PATH}/${ID}" \
   --include \
