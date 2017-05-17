29fb072225b07334ef5ab7102de37f589053462e
andyjzhong <andy.zhong@libertymutual.com>
2017-05-17 07:01:59 -0400

    Renamed Page Curl Scripts (Index & Show)
    Originally were called getOne and getMany, but changed each curl script
    to SHOW and INDEX respectively. Also removed the redundant show curl
    script.

--- scripts/pages/show.sh
+++ scripts/pages/show.sh
@@ -1,17 +1,15 @@
-#sh scripts/pages/show.sh
+#sh scripts/pages/getOne.sh
 
 #!/bin/sh
 
 API="http://localhost:4741"
 URL_PATH="/pages"
-TOKEN="np5/wbCPMRkwM3hl+pIPx+TrQSHetZgItaatrP8gWqk=--2gkVQNh8CObgqwKOw0omTL5/K02T/YTdJVJzlq7yS1Q="
-ID="validIdHere"
-
+#ID of a Specific Page
+TOKEN="PKovx5SovaSpolfE76ado9Vu+UD2X/ihhE35BDqY3xM=--PHIllUNmbKmXmQiFh8h8JIn4ad1jKLGMnA3z0fW4syk="
+ID="591b3ded212207f9bb92fab6"
 curl "${API}${URL_PATH}/${ID}" \
   --include \
   --request GET \
   --header "Authorization: Token token=$TOKEN"
 
 echo
-
-# Have not changed this yet.
